e7ada6e8ce8c4dff107930cd9bcd662c7448253f
jthw1005 <hjt105@gmail.com>
2025-12-05 20:38:50 +0900

    chore: 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+
+const base: string =
+  process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-2/' : '';
 
 export default mergeConfig(
   defineConfig({
+    base,
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          origin: resolve(__dirname, 'index.origin.html'),
+          basic: resolve(__dirname, 'index.basic.html'),
+          advanced: resolve(__dirname, 'index.advanced.html'),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
